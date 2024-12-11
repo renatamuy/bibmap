4e91f39679855d4c526a57c352dd386b91b5811e
Renata Muylaert <renatamuy@gmail.com>
2024-12-11 15:50:16 +1300

    figs

--- code/Fig_01_Figure_02_trends.R
+++ code/Fig_01_Figure_02_trends.R
@@ -1,21 +1,37 @@
+# Muylaert et al 
+# bat-network research review
+# R version 4.4.1 
+# Race for Your Life  
 
 require(RColorBrewer)
 require(tidyverse)
 library(forcats)
 require(Manu)
 library(rnaturalearth)
-library(rnaturalearthdat
-        
+library(rnaturalearthdata)
+library(here)        
+
 setwd('data')
+
 list.files()
 
-df <- xlsx::read.xlsx("bibmap_variables_prelim.xlsx", sheetIndex = 1, startRow=1)
+df <- xlsx::read.xlsx("bibmap_variables_prelim2.xlsx", sheetIndex = 1, startRow=1)
 
-df <- df[1:135,] #valid rows
+tail(df[1:133,])
+df[1,1:16]
 
+df <- df[1:133, 1:16] #valid rows
+
+table(df$Reference)
 tail(df)
+
+# Double checking the data
+
+length(unique(df$Number))
+
+length(unique(df$Reference))
+
 # Figure 1
-unique(df$Reference)
 
 df$Links_EE
 df$Global.South
@@ -23,9 +39,9 @@ df$Networks
 
 df$Links_EE <- fct_infreq(df$Links_EE)
 
-# Figure 1
+# Figure 2 
 
-setwd('./figures')
+setwd('../figures')
 
 table(df$Global.South)
 
@@ -33,12 +49,12 @@ table(df$Global.South)
 df$Category <- with(df, case_when(
   grepl("virus|viral|microbiome|parasite|rabies|Hendra|ectoparasite", Links_detail, ignore.case = TRUE) ~ "Host-pathogen \n interactions",
   grepl("species|genotype|genetic|evolutionary", Links_detail, ignore.case = TRUE) ~ "Metacommunities",
-  grepl("frugivory|nectarivory|seed dispersal|pollination|feeding", Links_detail, ignore.case = TRUE) ~ "Mutualistic interactions, \n frugivory and nectarivory",
-  grepl("social|behavior|roosts|shared|foraging|reproduction", Links_detail, ignore.case = TRUE) ~ "Bat social interactions",
+  grepl("frugivory|nectarivory|seed dispersal|pollination|feeding", Links_detail, ignore.case = TRUE) ~ "Mutualistic interactions",
+  grepl("social|behavior|roosts|shared|foraging|reproduction", Links_detail, ignore.case = TRUE) ~ "Bat societies",
   grepl("use of|landscape|resource|corridors|tents|roost", Links_detail, ignore.case = TRUE) ~ "Use of space",
   grepl("coauthorship|transmission|ecological|network|physical contact", Links_detail, ignore.case = TRUE) ~ "Social-ecological \n networks",
   grepl("predation", Links_detail, ignore.case = TRUE) ~ "Predation (insectivory)",
-  TRUE ~ NA_character_ # For cases that don't match any category
+  TRUE ~ NA_character_ # exception
 ))
 
 data.frame(df$Links_detail, df$Category)
@@ -59,8 +75,6 @@ unique(df1$Family)
 
 table(df$Links_detail)
 
-#--- export
-
 colnames(df1)
 
 table(df1$Global.South) 
@@ -69,13 +83,12 @@ table(df1$Mode)
 
 table(df1$Weight)
 
-
-
 df1 <- df1 %>% 
   filter(!is.na(Mode), !is.na(Category))
 
+#--- export fig
 
-jpeg(filename = 'Figure_02_Mode.jpg', res = 400, units = 'cm', width = 22, height = 16)
+jpeg(filename = 'Figure_02.jpg', res = 400, units = 'cm', width = 22, height = 16)
 
  ggplot(df1, aes(x = Category, fill = Mode)) +
   geom_bar() + 
@@ -83,10 +96,10 @@ jpeg(filename = 'Figure_02_Mode.jpg', res = 400, units = 'cm', width = 22, heigh
   labs(
     title = "Ecological Network Studies",
     x = "Topic",
-    y = "Frequency",
-    fill = "Network" 
+    y = "Number of studies",
+    fill = "Network type" 
   ) +
-   scale_fill_manual(values = get_pal("Pohutukawa") ) +
+   scale_fill_manual(values = get_pal("Kereru") ) +
    #scale_fill_viridis(discrete = TRUE) + 
   theme_minimal() +
   theme(
@@ -95,7 +108,7 @@ jpeg(filename = 'Figure_02_Mode.jpg', res = 400, units = 'cm', width = 22, heigh
 
 dev.off()
 
-# Figure 2 - growth
+# Figure 1- growth
 
 studies_per_year <- df %>%
   group_by(Year) %>%
@@ -107,7 +120,7 @@ studies_per_2years <- df %>%
   group_by(Year) %>%                  
   summarise(Count = n())      
 
-
+# Fig 1
 setwd('../figures')
 
 jpeg(filename = 'Figure_01.jpg', res = 400, units = 'cm', width = 14, height = 10 )
@@ -133,14 +146,28 @@ dev.off()
 
 table(df$Country_or_Region)
 
-# Recode df$Country_or_Region to df$Geo matching their info to a country in rnaturalearth so I can make a map plot in R
+unique(df$Country_or_Region)
 
-df$Country_or_Region
+df <- df %>%
+  mutate(Geo_general = ifelse(grepl(" and ", Country_or_Region), "Multicountry", Country_or_Region))
+
+# go on with recoding
+
+df <- df %>%
+  mutate(Geo_general = ifelse(grepl("Neotropical", Geo_general), "Multicountry", Geo_general))
+
+df <- df %>%
+  mutate(Geo_general = ifelse(grepl("North Africa", Geo_general), "Multicountry", Geo_general))
+
+# check
+unique(df$Geo_general)
+
+# other option for countries
 
 df <- df %>%
   mutate(
     Geo = case_when(
-      Country_or_Region == "USA" ~ "United States",
+      Country_or_Region == "USA" ~ "United States of America",
       Country_or_Region == "England" ~ "United Kingdom",
       Country_or_Region == "Ucrane and Russia and Azerbaijan" ~ "Ukraine, Russia, Azerbaijan",
       Country_or_Region == "Bahamas and Greater Antilles and Lesser Antilles" ~ "Caribbean",
@@ -159,37 +186,50 @@ df <- df %>%
   )
 
 
-dfc <- Geo %>%
-  group_by(Country_or_Region) %>%
+dfc <- df %>%
+  group_by(Geo_general) %>%
   summarise(Count = n()) %>% 
   arrange(desc(Count))
 
+dfc
 
 tail(dfc)
 
 dfc <- dfc %>% 
-  filter(!is.na(Country_or_Region)) %>% 
+  filter(!is.na(Geo_general)) %>% 
   arrange(Count)
 
+# export table 
 xlsx::write.xlsx(dfc, "Table_studies_per_country.xlsx")
 
-# Figure 
+# Figure03
 
-
-dfc$Country_or_Region <- reorder(dfc$Country_or_Region, -dfc$Count)
+dfc$Geo_general <- reorder(dfc$Geo_general, -dfc$Count)
 
 # export
 jpeg(filename = 'Figure_03.jpg', res = 400, units = 'cm', width = 24, height = 20 )
 
-ggplot(dfc, aes(x = Country_or_Region, y = Count, fill = Country_or_Region)) +
-  geom_bar(stat = "identity", fill='royalblue', show.legend = FALSE) + 
+dfc$Geo_general
+
+dfc <- dfc %>%
+  mutate(
+    fill_color = ifelse(Geo_general %in% c("Global", "Multicountry", "In silico"),  "#c47c94", "royalblue")
+  )
+
+
+ggplot(dfc, aes(x = Geo_general, y = Count, fill = fill_color)) +
+  geom_bar(
+    stat = "identity", 
+    show.legend = FALSE
+  ) + 
+  scale_fill_identity() +  # Directly maps the fill_color column
   coord_flip() + 
   labs(
     title = "",
     x = "Country or Region",
-    y = "Count"
+    y = "Number of studies"
   ) +
-   theme_minimal() +
+  theme_minimal() +
   theme(
     axis.text.x = element_text(size = 10),
     axis.text.y = element_text(size = 10),
@@ -198,28 +238,28 @@ ggplot(dfc, aes(x = Country_or_Region, y = Count, fill = Country_or_Region)) +
 
 dev.off()
 
-# Map
-
-# removing studies with global or vague regional mentions
+# Map - removing studies with global or vague regional mentions (keeping only named countries)
 
 country_hits <- df %>%
   filter(!is.na(Geo)) %>% 
   separate_rows(Geo, sep = ", ") %>% 
-  filter(!Geo %in% c("Global", "Neotropical", "Neotropics and Europe", "North Africa", "Europe")) %>% # Exclude vague regions
+  filter(!Geo %in% c('In silico', "Global", "Neotropical", "Neotropics and Europe", "North Africa", "Europe")) %>% # Exclude vague regions
   group_by(Geo) %>%
   summarize(Hits = n()) %>%
   ungroup()
 
-country_hits
+tail(country_hits)
 
 
 world <- ne_countries(scale = "medium", returnclass = "sf")
+
+world$name
 
 map_data <- world %>%
   left_join(country_hits, by = c("name" = "Geo"))
 
 #
-jpeg(filename = 'Figure_03_Map.jpg', res = 400, units = 'cm', width = 14, height = 10 )
+jpeg(filename = 'Figure_S4.jpg', res = 400, units = 'cm', width = 14, height = 10 )
 
 ggplot(map_data) +
   geom_sf(aes(fill = Hits), color = "gray70", size = 0.2) +
